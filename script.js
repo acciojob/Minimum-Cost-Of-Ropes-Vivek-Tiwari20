9a30fc976c728e3c7952fa92cdaea6153b629641
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-05-01 15:02:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 function calculateMinCost() {
-  const ropeLengthsStr = document.getElementById('rope-lengths').value;
-  const ropeLengthsArr = ropeLengthsStr.split(',').map(Number);
-  const n = ropeLengthsArr.length;
+  const str = document.querySelector('#rope-lengths').value;
+  const strArr = str.split(',');
+  const arr = strArr.map(e => Number.parseInt(e));
+ 
+  let n = arr.length;
+ 
+  let ans = helper(arr, n);
+  document.querySelector('#result').innerText = ans;
+}
+ 
+function helper(arr, n) {
+  // Create a priority queue
   let pq = [];
+ 
+  // Adding items to the pQueue
   for (let i = 0; i < n; i++) {
-    pq.push(ropeLengthsArr[i]);
+    pq.push(arr[i]);
   }
-  pq.sort(function (a, b) { return a - b; });
+ 
+  pq.sort(function(a, b) {
+    return a - b;
+  });
+ 
+  // Initialize result
   let res = 0;
+ 
+  // While size of priority queue
+  // is more than 1
   while (pq.length > 1) {
+    // Extract shortest two ropes from pq
     let first = pq.shift();
     let second = pq.shift();
+ 
+    // Connect the ropes: update result
+    // and insert the new rope to pq
     res += first + second;
     pq.push(first + second);
-    pq.sort(function (a, b) { return a - b; });
+ 
+    pq.sort(function(a, b) {
+      return a - b;
+    });
   }
-  document.getElementById('result').textContent = res;
+  return res;
 }
+ 
